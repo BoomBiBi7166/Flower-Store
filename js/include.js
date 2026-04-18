be514a20894c7b822bee26230bbd87dd5eb9fecd
Yanawut Sorntavorn <168202423+BoomBiBi7166@users.noreply.github.com>
2026-04-19 01:30:51 +0700

    make navbar, footer work

--- js/include.js
+++ js/include.js
@@ -1,9 +1,9 @@
-fetch("/partials/navbar.html")
+fetch("/Flower-Store/partials/navbar.html")
     .then(res => res.text())
     .then(html => {
         document.getElementById("navbar").innerHTML = html;
     });
-fetch("/partials/footer.html")
+fetch("/Flower-Store/partials/footer.html")
     .then(res => res.text())
     .then(html => {
         document.getElementById("footer").innerHTML = html;
