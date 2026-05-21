d79ae09ad6f505071844d67b2498ca7ebb0957d2
Yanawut Sorntavorn <168202423+BoomBiBi7166@users.noreply.github.com>
2026-05-21 17:02:35 +0700

    Add product.js
    Add product.js

---
+++ js/product.js
@@ -0,0 +1,65 @@
+// READ URL
+const params =
+    new URLSearchParams(window.location.search);
+
+const productID =
+    params.get("id");
+
+
+// LOAD PRODUCTS FROM JSON
+fetch("../products.json")
+
+    .then(res => res.json())
+
+    .then(data => {
+
+        const product =
+            data.find(p => p.id === productID);
+
+        if (product) {
+
+            document.getElementById("productName").innerText =
+                product.name;
+
+            document.getElementById("productPrice").innerText =
+                "฿" + product.price;
+
+            document.getElementById("productCollection").innerText =
+                product.collection;
+
+            document.getElementById("productDescription").innerText =
+                product.description;
+
+            document.getElementById("productImage").src =
+                product.image;
+
+            document.getElementById("breadcrumbName").innerText =
+                product.name;
+                
+        }
+
+    });
+
+
+// QUANTITY BUTTONS
+function increaseQuantity() {
+
+    const input =
+        document.getElementById("quantity");
+
+    input.value =
+        parseInt(input.value) + 1;
+}
+
+
+function decreaseQuantity() {
+
+    const input =
+        document.getElementById("quantity");
+
+    if (parseInt(input.value) > 1) {
+
+        input.value =
+            parseInt(input.value) - 1;
+    }
+}
